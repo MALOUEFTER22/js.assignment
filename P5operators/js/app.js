8341554322649732b957dcf934d39797768c754d
MALOUEFTER22 <yehyaali61@outlook.com>
2024-09-12 14:52:10 +0200

    carti

---
+++ P5operators/js/app.js
@@ -0,0 +1,8 @@
+let speedinput = 72;
+let speedoutput = 0;
+speedoutput = speedinput / 3.6;
+console.log(speedoutput);
+let minutesinput = 3;
+let secondsoutput = 0;
+//.....
+console.log(secondsoutput);
---
+++ P5operators/js/app2.js
@@ -0,0 +1,10 @@
+let x = 5 + 5; // x = 10
+let y = x + 5; // y = 15
+let z = y + y; // z = 30
+console.log (z+z); //60
+
+let gold = 50;
+x = gold + 10;
+console.log(x);
+goldx = gold + 10;
+console.log(goldx );
