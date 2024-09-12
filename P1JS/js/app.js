f06b74a0ec69f7a62f03f58d6b077f8763d6ecc6
MALOUEFTER22 <yehyaali61@outlook.com>
2024-09-12 11:40:48 +0200

    lunch

---
+++ P1JS/js/app.js
@@ -0,0 +1,18 @@
+let numberofbottles = 10;
+const line1 = "Greenbottles hanging on the wall";
+const line2 = "and if one green bottle, should accidentally fall there will be 9";
+
+console.log(numberofbottles);
+console.log(line1);
+console.log(numberofbottles);
+console.log(line1);
+console.log(line2);
+-- numberofbottles;
+console.log(numberofbottles);
+console.log(line1);
+-- numberofbottles;
+console.log(numberofbottles)
+-- numberofbottles
+
+
+
